e2b15fb4bb60884f7de1e9ebe6053cdd571ff081
Danil Mannanov <vildanmannanov2@gmail.com>
2026-01-18 23:53:04 +0500

    Структура проекта

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -6,6 +6,12 @@ namespace Notes.Domain
 {
     internal class Note
     {
-        
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EditDate { get; set; }
+
     }
 }
