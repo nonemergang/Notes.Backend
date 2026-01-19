93573bc9c9da119a6519618a68e960296a4f4576
Danil Mannanov <vildanmannanov2@gmail.com>
2026-01-19 23:32:25 +0500

    ..

---
+++ Notes.Application/Api.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore;
+
+namespace Notes.Application
+{
+    internal class Api
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/" => "Hello");
+
+        app.Run();
+    }
+}
